510deb9fe85572d7bede7fa7929241f6d8a0c89c
Fanny Jiang <jiangf@bu.edu>
2017-09-08 07:06:25 +0800

    able to delete user-added lists

--- app/components/new-list.js
+++ app/components/new-list.js
@@ -20,5 +20,8 @@ export default Ember.Component.extend({
     showDescription(listItem) {
       this.get('onClick')(listItem);
     },
+    deleteList(listName) {
+      document.getElementById(listName).remove();
+    }
   }
 });
